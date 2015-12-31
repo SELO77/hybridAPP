d66bd6a0941fea1d4809a5e4908052b0c198a0a9
siyongkim <ksy5704@naver.com>
2015-12-31 11:05:04 +0900

    [김시용] 1. friend Diary 추가

--- hybridApp/webapp/app/js/diary/diary.js
+++ hybridApp/webapp/app/js/diary/diary.js
@@ -167,6 +167,173 @@ function loadDiary(args) {
 					});
 }
 
+//[시용]friend Diary
+function friendDiary(args) {
+	
+	$
+			.ajax(
+					requestURL + 'getUserDiaryList',
+					{
+						method : "post",
+						crossDomain : true,
+						dataType : "json",
+						contentType : "application/json; charset=UTF-8",
+						data : JSON.stringify({
+							uNo : args
+						}),
+						success : function(result) {
+							// alert("Ajax success");
+							var temp = diaryLink.import;
+							console.log(result);
+							var source = temp.querySelector('#entry-template').text;
+							Handlebars
+									.registerHelper(
+											'userCheck',
+											function(uNo, dNo) {
+												// alert(rUNo);
+												var userhtml = "<div class='dropdown pull-right' style='padding-right: 20px;padding-top: 10px;'><i class='fa fa-cog'data-toggle='dropdown'/><div class='dropdown-menu' role='menu' aria-labelledby='dLabel' style='padding-top: 0px;padding-bottom: 0px;'><p style='margin-bottom: 0px;'><input type='hidden' value='"
+														+ dNo
+														+ "'><updateDiary style='height:transparent'><button type='button' class='btn btn-default'  style='width:100%'><a>수정하기</a></button></updateDiary></p><p style='margin-bottom: 0px;'><input type='hidden' value='"
+														+ dNo
+														+ "'><deleteDiary style='height:transparent'><button type='button' class='btn btn-default' style='width:100%'><a>삭제하기</a></button></deleteDiary></p></div></div>";
+
+												if (uNo == user.uNo) {
+													return userhtml;
+												} else {
+													return '';
+												}
+
+											});
+
+							// [SELO77] dContent에서 Tag효과 주는 로직 추가.
+							Handlebars
+									.registerHelper(
+											'dContentForTag',
+											function(dContent) {
+												console
+														.log('== dContent Parsing for HashTag == dContent:'
+																+ dContent);
+
+												var startTagIndex = dContent
+														.indexOf("#");
+												var temp = dContent
+														.slice(startTagIndex);
+												var endTagIndex = temp
+														.indexOf(" ");
+
+												var hashTags = temp.slice(0,
+														endTagIndex);
+												console.log('hashTags :'
+														+ hashTags);
+
+												var hashTagList = hashTags
+														.split("#");
+												hashTagList.splice(0, 1);
+
+												for (i in hashTagList) {
+													var temp = '<strong style="color:#125688" class="hashTag">#'
+															+ hashTagList[i]
+															+ '</strong>';
+													hashTagList[i] = temp;
+												}
+
+												return dContent.replace(
+														hashTags, hashTagList
+																.toString()
+																.replace(/,/gi,
+																		""));
+											}); // /Handlebars.registerHelper('dContentForTag'
+
+							Handlebars
+									.registerHelper(
+											'likeCheck',
+											function(likeList) {
+												// alert(rUNo);
+												var likehtml = "<insertLike><a><i class='fa fa-thumbs-o-up'/> 좋아요</a></insertLike>";
+
+												for (var i = 0; i < likeList.length; i++) {
+													if (likeList[i].uNo == user.uNo
+															&& likeList[i].lState == 1) {
+														likehtml = "<deleteLike><a><i class='fa fa-thumbs-o-up'/> 좋아요취소</a></deleteLike>";
+													}
+												}
+												return likehtml;
+											});
+							Handlebars
+									.registerHelper(
+											'sharePicCheck',
+											function(dPics) {
+												// alert(rUNo);
+												var returnImg = '';
+												if (dPics.length == 0
+														|| dPics[0].trim() == '') {
+													return returnImg;
+												} else {
+													for (var i = 0; i < dPics.length; i++) {
+														if (i != dPics.length - 1) {
+															returnImg += imgSrc
+																	+ dPics[i]
+																	+ ',';
+														} else {
+															returnImg += imgSrc
+																	+ dPics[i];
+														}
+													}
+													return returnImg;
+												}
+											});
+
+							Handlebars
+									.registerHelper(
+											'picCheck',
+											function(dPics) {
+												// alert(rUNo);
+												var returnHtml = ' ';
+												if (dPics.length == 0
+														|| dPics[0].trim() == '') {
+													return returnHtml;
+												}
+												if (dPics.length == 1) {
+													returnHtml = '<img class="img" src="'
+															+ imgSrc
+															+ dPics[0]
+															+ '" style="width: 100%; height:auto; margin: 0">';
+													return returnHtml;
+												} else {
+													if (dPics.length % 2 == 0) {
+
+														for (var i = 0; i < dPics.length; i++) {
+															returnHtml += '<img class="img" src="'
+																	+ imgSrc
+																	+ dPics[i]
+																	+ '" style="width: 50%; height:auto; padding: 3px; ">';
+														}
+
+													} else {
+														returnHtml += '<img class="img" src="'
+																+ imgSrc
+																+ dPics[0]
+																+ '" style="width: 100%; height:auto;">';
+
+														for (var i = 1; i < dPics.length; i++) {
+															returnHtml += '<img class="img" src="'
+																	+ imgSrc
+																	+ dPics[i]
+																	+ '" style="width: 50%; height:auto; padding: 3px">';
+														}
+													}
+													return returnHtml;
+												}
+											});
+							var template = Handlebars.compile(source);
+							var html = template(result);
+							// $('#diaryHandle').append(html);
+							$('#diaryHandle').after(html);
+							// }
+						}
+					});
+}
+
 $(document).on(
 		'tap',
 		'#shareDiary',
